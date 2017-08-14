12984c5ba263d62284b7c51ecc4b17137c9af8d4
Milos <zbiljicmilos@gmail.com>
2017-08-14 21:25:47 +0200

    Middleware

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -8,7 +8,7 @@ class LoginController extends Controller
 {
 	public function __construct()
 	{
-		$this->middleware('guest');
+		$this->middleware('guest', ['except' => 'destroy']);
 	}
 
     public function destroy()
