7587e61b7a97ed82023b768631c450f5d3024362
Milos <zbiljicmilos@gmail.com>
2017-08-16 21:31:11 +0200

    Tags added

--- app/Post.php
+++ app/Post.php
@@ -6,7 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
 
     public function comments()
     {
